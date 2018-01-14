487a400af07beb3262a9fdc060b67a529d23cc8d
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-14 03:47:19 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activate
+    set_student
+    if @student.active == true
+      @student.active = false
+    else
+      @student.active = true
+    end
+  end
 end
